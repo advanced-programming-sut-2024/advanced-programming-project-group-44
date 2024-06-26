1c2c0677436f7830f0df7183db1e7df06fe1ecdf
aryana <aryana.zallnezhad@gmail.com>
2024-06-26 15:54:27 +0330

    user saved in app

--- src/main/java/com/ap/gwentgame/App.java
+++ src/main/java/com/ap/gwentgame/App.java
@@ -1,8 +1,12 @@
 package com.ap.gwentgame;
 
+import com.ap.gwentgame.model.User;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class App {
+    private static ArrayList<User> allusers = new ArrayList<>();
     private static Stage stage;
 
     public static Stage getStage() {
@@ -11,5 +15,18 @@ public class App {
 
     public static void setStage(Stage stage) {
         App.stage = stage;
+    }
+
+    public static User getUserByName(String name){
+        for(User user : allusers){
+            if(user.getName().equals(name)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static void addUser(User user){
+        allusers.add(user);
     }
 }
