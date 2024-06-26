ffa0beba061fe9961bb2288983be717ca0f12742
parmis <parmis.hemasian@gmail.com>
2024-06-26 12:14:48 +0330

    Stage management system changed

--- src/main/java/com/ap/gwentgame/App.java
+++ src/main/java/com/ap/gwentgame/App.java
@@ -3,26 +3,13 @@ package com.ap.gwentgame;
 import javafx.stage.Stage;
 
 public class App {
-    private Stage stage;
-    private static App instance;
+    private static Stage stage;
 
-    private App(Stage stage) {
-        this.stage = stage;
-    }
-
-    public static void runApplication(Stage stage) {
-        instance = new App(stage);
-    }
-
-    public static App getInstance() {
-        return instance;
-    }
-
-    public Stage getStage() {
+    public static Stage getStage() {
         return stage;
     }
 
-    public void setStage(Stage stage) {
-        this.stage = stage;
+    public static void setStage(Stage stage) {
+        App.stage = stage;
     }
 }
