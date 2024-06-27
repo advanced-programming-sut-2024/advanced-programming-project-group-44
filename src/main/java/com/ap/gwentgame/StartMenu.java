f06f3a2597a9b05c446e5ccc8f6442da27866fdd
aryana <aryana.zallnezhad@gmail.com>
2024-06-27 04:16:19 +0330

    setting up the StartMenu StartMenuController and StartMenu.fxml

--- src/main/java/com/ap/gwentgame/StartMenu.java
+++ src/main/java/com/ap/gwentgame/StartMenu.java
@@ -1,18 +1,35 @@
 package com.ap.gwentgame;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class StartMenu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        App.runApplication(stage);
+        App.setStage(stage);
+
+//        Image icon = new Image(getClass().getResourceAsStream("/icon/rocket (3).png"));
+//        stage.getIcons().add(icon);
+
+        URL url = StartMenu.class.getResource("/com/ap/gwentgame/fxml/StartMenu.fxml");
+        Pane root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
+
 }
