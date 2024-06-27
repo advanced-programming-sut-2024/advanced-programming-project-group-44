eeaf36805e5bfea25af328a6e23128d3df25e032
sobhanaghasi <sobhanaghasi1384@gmail.com>
2024-06-27 11:34:41 +0330

    Utilities class created

---
+++ src/main/java/com/ap/gwentgame/Utilities.java
@@ -0,0 +1,7 @@
+package com.ap.gwentgame;
+
+public class Utilities {
+    public static String getResourcePath(String path){
+        return Utilities.class.getResource("/com/ap/gwentgame/" + path).toExternalForm();
+    }
+}
