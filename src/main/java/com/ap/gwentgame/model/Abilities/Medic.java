49a1a505e159b20dba4c9d389ce13e1f3632eec8
aryana <aryana.zallnezhad@gmail.com>
2024-06-28 17:58:27 +0330

    medic ability completed

--- src/main/java/com/ap/gwentgame/model/Abilities/Medic.java
+++ src/main/java/com/ap/gwentgame/model/Abilities/Medic.java
@@ -1,8 +1,29 @@
 package com.ap.gwentgame.model.Abilities;
 
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Medic extends Ability{
+    public Medic(Card card) {
+        super(card);
+    }
+
     @Override
-    public void run() {
-        // TODO implement here
+    public void run(Board board) {
+        Player player = board.getCurrentPlayer();
+        ArrayList<Card> discardCards = player.getDiscardPile();
+        Card randomCard = getRandomCard(discardCards);
+        player.addCardToHand(randomCard);
+
+    }
+    public static Card getRandomCard(ArrayList<Card> list) {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(list.size());
+        return list.get(randomIndex);
     }
 }
