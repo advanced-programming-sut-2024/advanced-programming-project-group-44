bd496072911f805040ca9d0b29539d75a0f266dd
aryana <aryana.zallnezhad@gmail.com>
2024-06-28 18:20:18 +0330

    Berserker and Transformers and Mardroeme changed but not completed

--- src/main/java/com/ap/gwentgame/model/Abilities/Berserker.java
+++ src/main/java/com/ap/gwentgame/model/Abilities/Berserker.java
@@ -1,8 +1,15 @@
 package com.ap.gwentgame.model.Abilities;
 
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Game.Board;
+
 public class Berserker extends Ability{
+    public Berserker(Card card) {
+        super(card);
+    }
+
     @Override
-    public void run() {
+    public void run(Board board) {
         // TODO implement here
     }
 }
