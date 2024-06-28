0a373b1507ec19fbc11a1c00405e127a716c0835
aryana <aryana.zallnezhad@gmail.com>
2024-06-28 18:01:45 +0330

    TightBond ability completed

--- src/main/java/com/ap/gwentgame/model/Abilities/TightBond.java
+++ src/main/java/com/ap/gwentgame/model/Abilities/TightBond.java
@@ -1,8 +1,31 @@
 package com.ap.gwentgame.model.Abilities;
 
-public class TightBond extends Ability{
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Cards.UnitCard;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
+public class TightBond extends Ability {
+    public TightBond(Card card) {
+        super(card);
+    }
+
     @Override
-    public void run() {
-        // TODO implement here
+    public void run(Board board) {
+        Player player = board.getCurrentPlayer();
+        int row = card.getPlacement().getRow();
+        int countOfTightBondCards = 0;
+        for (Card targetCard : player.getRows()[row]) {
+            if (targetCard.getAbility() instanceof TightBond) {
+                countOfTightBondCards++;
+            }
+        }
+        int newScoreForTightBondCards = countOfTightBondCards * ((UnitCard) card).getScore();
+        for (Card targetCard : player.getRows()[row]) {
+            if (targetCard.getAbility() instanceof TightBond) {
+                UnitCard unitcard = ((UnitCard) targetCard);
+                unitcard.setScore(newScoreForTightBondCards);
+            }
+        }
     }
 }
