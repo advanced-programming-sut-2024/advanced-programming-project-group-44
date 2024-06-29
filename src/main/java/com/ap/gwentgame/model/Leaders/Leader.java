7012959f23ebf06e3d65c0d3264c4e3d73bc63fe
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:09:30 +0330

    added Board board to execute ability

--- src/main/java/com/ap/gwentgame/model/Leaders/Leader.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/Leader.java
@@ -1,9 +1,10 @@
 package com.ap.gwentgame.model.Leaders;
 
 import com.ap.gwentgame.model.Cards.Item;
+import com.ap.gwentgame.model.Game.Board;
 
 public abstract class Leader extends Item {
-    public abstract void executeAbility();
+    public abstract void executeAbility(Board board);
 
     public Leader(String name) {
         super(name);
