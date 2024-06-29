2a5491a4e095508adb86f9bc593a01ef76f0c3e7
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:07:17 +0330

    TheSteelForged completed but must change

--- src/main/java/com/ap/gwentgame/model/Leaders/TheSteelForged.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/TheSteelForged.java
@@ -1,12 +1,21 @@
 package com.ap.gwentgame.model.Leaders;
 
+import com.ap.gwentgame.model.Cards.Weather;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
 public class TheSteelForged extends Leader{
     public TheSteelForged(String name){
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board){
+        Weather weatherCard = board.getWeatherCard();
+        if(weatherCard != null){
+            board.setWeatherCard(null);
+            Player player = board.getCurrentPlayer();
+            player.addWeatherCardToDiscardPile(weatherCard);
+        }
     }
 }
