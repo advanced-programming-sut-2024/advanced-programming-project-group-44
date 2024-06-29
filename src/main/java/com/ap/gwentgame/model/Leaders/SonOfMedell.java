da64c72b801fcf7ae22c0bed94200092090b36a6
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:06:09 +0330

    SonOfMedell completed but must change

--- src/main/java/com/ap/gwentgame/model/Leaders/SonOfMedell.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/SonOfMedell.java
@@ -1,12 +1,33 @@
 package com.ap.gwentgame.model.Leaders;
 
-public class SonOfMedell extends Leader{
-    public SonOfMedell(String name){
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Cards.UnitCard;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
+public class SonOfMedell extends Leader {
+    public SonOfMedell(String name) {
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board) {
+        Player opponent = board.getOpponentPlayer();
+        int score = 0;
+        int maxScore = 0;
+        for (Card card : opponent.getRows()[1]) {
+            if (card instanceof UnitCard && !((UnitCard) card).isHero()) {
+                score += ((UnitCard) card).getScore();
+                if (((UnitCard) card).getScore() > maxScore) maxScore = ((UnitCard) card).getScore();
+            }
+        }
+        if (score >= 10) {
+            for (Card card : opponent.getRows()[1]) {
+                if (card instanceof UnitCard && !((UnitCard) card).isHero()
+                        && ((UnitCard) card).getScore() == maxScore) {
+                    opponent.addCardToDiscardPile(card, 1);
+                }
+            }
+        }
     }
 }
