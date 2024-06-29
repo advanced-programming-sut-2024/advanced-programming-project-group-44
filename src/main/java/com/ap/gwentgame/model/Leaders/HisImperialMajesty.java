cbc57ade7ffdd2a63cf545b0e5cd09bfe15528e7
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:02:56 +0330

    HisImperialMajesty completed ut must change

--- src/main/java/com/ap/gwentgame/model/Leaders/HisImperialMajesty.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/HisImperialMajesty.java
@@ -1,12 +1,43 @@
 package com.ap.gwentgame.model.Leaders;
 
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
+import java.lang.foreign.PaddingLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class HisImperialMajesty extends Leader{
     public HisImperialMajesty(String name){
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board){
+        Player opponent = board.getOpponentPlayer();
+        ArrayList<Card> handCardsOpponent = opponent.getHand();
+        int handCardsOpponentNum = handCardsOpponent.size();
+        ArrayList<Card> randomCards;
+        if(handCardsOpponentNum < 4) randomCards = getRandomCards(handCardsOpponent , handCardsOpponentNum);
+        else randomCards = getRandomCards(handCardsOpponent , 3);
+        //TODO shoe them to the player
+
+    }
+    public static ArrayList<Card> getRandomCards(ArrayList<Card> list, int numberOfCards) {
+        if (numberOfCards > list.size()) {
+            throw new IllegalArgumentException("Number of cards requested exceeds the size of the list");
+        }
+
+        ArrayList<Card> copy = new ArrayList<>(list);
+        Collections.shuffle(copy);
+
+        ArrayList<Card> result = new ArrayList<>();
+        for (int i = 0; i < numberOfCards; i++) {
+            result.add(copy.get(i));
+        }
+
+        return result;
     }
 }
