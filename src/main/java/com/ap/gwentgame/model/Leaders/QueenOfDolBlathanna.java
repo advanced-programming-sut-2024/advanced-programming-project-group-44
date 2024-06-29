28746c004d147e0ccbc9e444b3e8393245b249e8
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:05:50 +0330

    QueenOfDolBlathanna completed but must change

--- src/main/java/com/ap/gwentgame/model/Leaders/QueenOfDolBlathanna.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/QueenOfDolBlathanna.java
@@ -1,12 +1,28 @@
 package com.ap.gwentgame.model.Leaders;
 
+import com.ap.gwentgame.Utilities;
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Cards.UnitCard;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
 public class QueenOfDolBlathanna extends Leader{
     public QueenOfDolBlathanna(String name){
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board){
+        Player opponent = board.getOpponentPlayer();
+        int score = Utilities.calculateScoreOfRowNotHero(opponent , 0);
+        if(score >= 10) {
+            int maxScore = Utilities.calculateMaxScoreOfRowNotHero(opponent, 1);
+            for (Card card : opponent.getRows()[1]) {
+                if (card instanceof UnitCard && !((UnitCard) card).isHero()
+                        && ((UnitCard) card).getScore() == maxScore) {
+                    opponent.addCardToDiscardPile(card, 1);
+                }
+            }
+        }
     }
 }
