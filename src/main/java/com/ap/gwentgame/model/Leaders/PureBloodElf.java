d9c351afc6f76176cf6c6e9d19944630288291ca
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:05:25 +0330

    PureBloodElf completed but must change

--- src/main/java/com/ap/gwentgame/model/Leaders/PureBloodElf.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/PureBloodElf.java
@@ -1,12 +1,24 @@
 package com.ap.gwentgame.model.Leaders;
 
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
+import java.util.ArrayList;
+
 public class PureBloodElf extends Leader{
     public PureBloodElf(String name) {
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board){
+        Player player = board.getCurrentPlayer();
+        ArrayList<Card> deckCards = player.getDeck();
+        for(Card card : deckCards){
+            if(card.getName().contains("frost")){
+                //TODO play the fog card
+            }
+        }
     }
 }
