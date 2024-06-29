d8a6b34bac782192196b54b7b3d1da8d45c77390
aryana <aryana.zallnezhad@gmail.com>
2024-06-30 03:06:31 +0330

    TheBeautiful completed but must change

--- src/main/java/com/ap/gwentgame/model/Leaders/TheBeautiful.java
+++ src/main/java/com/ap/gwentgame/model/Leaders/TheBeautiful.java
@@ -1,12 +1,25 @@
 package com.ap.gwentgame.model.Leaders;
 
+import com.ap.gwentgame.model.Abilities.CommandersHorn;
+import com.ap.gwentgame.model.Cards.Card;
+import com.ap.gwentgame.model.Cards.UnitCard;
+import com.ap.gwentgame.model.Game.Board;
+import com.ap.gwentgame.model.Game.Player;
+
 public class TheBeautiful extends Leader{
     public TheBeautiful(String name){
         super(name);
     }
 
     @Override
-    public void executeAbility(){
-        //TODO implement here
+    public void executeAbility(Board board){
+        Player player = board.getCurrentPlayer();
+        //TODO check the row & check if it also happens for the opponent
+        for(Card card : player.getRows()[1]){
+            if(card instanceof UnitCard && !(card.getName().contains("commander'sHorn")) &&
+                    !((UnitCard) card).isHero()){
+                ((UnitCard) card).setScore(((UnitCard) card).getScore() * 2);
+            }
+        }
     }
 }
