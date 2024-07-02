886baedc9e9fe2c27f5ad385e8eb5d52855fe4d4
sobhanaghasi <sobhanaghasi1384@gmail.com>
2024-07-02 12:21:21 +0330

    Added pane support to ItemContainer

--- src/main/java/com/ap/gwentgame/model/ItemContainer.java
+++ src/main/java/com/ap/gwentgame/model/ItemContainer.java
@@ -4,43 +4,47 @@ import com.ap.gwentgame.model.Cards.Item;
 import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
 
-public class CardContainer<T extends Item> extends FlowPane {
-    public CardContainer() {
+public class ItemContainer<T extends Item> extends FlowPane {
+    public ItemContainer() {
         this.orientationProperty().setValue(Orientation.HORIZONTAL);
     }
 
-    public void setVisuals(int x, int y, int width, int height, int horizontalGap, int verticalGap) {
+    public void setVisuals(Pane pane, int x, int y, int width, int height, int horizontalGap, int verticalGap) {
         this.setLayoutX(x);
         this.setLayoutY(y);
         this.setPrefWidth(width);
         this.setPrefHeight(height);
         this.setHgap(horizontalGap);
         this.setVgap(verticalGap);
+        pane.getChildren().add(this);
     }
 
-    public void addCard(T card) {
-        this.getChildren().add(card);
+    public void add(T item) {
+        this.getChildren().add(item);
     }
 
-    public void removeCard(T card) {
-        this.getChildren().remove(card);
+    public void remove(T item) {
+        this.getChildren().remove(item);
     }
 
     public void clear() {
         this.getChildren().clear();
     }
 
-    public ArrayList<T> getCards() {
+    public ArrayList<T> getItems() {
         ArrayList<T> cards = new ArrayList<>();
         for (int i = 0; i < this.getChildren().size(); i++) {
             Node node = this.getChildren().get(i);
             try {
-                cards.add((T) node);
+                if (node instanceof Item) {
+                    cards.add((T) node);
+                }
             } catch (ClassCastException e) {
-                System.out.println("ClassCastException in CardContainer");
+                System.out.println("ClassCastException in ItemContainer");
             }
         }
         return cards;
