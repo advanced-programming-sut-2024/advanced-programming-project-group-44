971bf8f25ca65fba6afe2dd36f4e902fd88faaab
aryana <aryana.zallnezhad@gmail.com>
2024-07-11 21:29:13 +0330

    dataBase

--- src/main/java/com/ap/gwentgame/client/model/GameLog.java
+++ src/main/java/com/ap/gwentgame/client/model/GameLog.java
@@ -1,51 +1,157 @@
 package com.ap.gwentgame.client.model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "game_log")
 public class GameLog {
-    private final String OponnentName;
-    private final Date date;
-    private final int playerScore;
-    private final int oponnentScore;
-    private final int[] playerScores;
-    private final int[] oponnentScores;
-    private final String winner;
 
-    public GameLog(String oponnentName, Date date, int playerScore, int oponnentScore, int[] playerScores, int[] oponnentScores, String winner) {
-        OponnentName = oponnentName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "opponent_name")
+    private String opponentName;
+
+    @Column(name = "game_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @Column(name = "player_score")
+    private int playerScore;
+
+    @Column(name = "opponent_score")
+    private int opponentScore;
+
+    @Column(name = "player_score_round1")
+    private int playerScoreRound1;
+
+    @Column(name = "player_score_round2")
+    private int playerScoreRound2;
+
+    @Column(name = "player_score_round3")
+    private int playerScoreRound3;
+
+    @Column(name = "opponent_score_round1")
+    private int opponentScoreRound1;
+
+    @Column(name = "opponent_score_round2")
+    private int opponentScoreRound2;
+
+    @Column(name = "opponent_score_round3")
+    private int opponentScoreRound3;
+
+    @Column(name = "winner")
+    private String winner;
+
+    public GameLog() {
+    }
+
+    public GameLog(String opponentName, Date date, int playerScore, int opponentScore, int[] playerScores, int[] opponentScores, String winner) {
+        this.opponentName = opponentName;
         this.date = date;
         this.playerScore = playerScore;
-        this.oponnentScore = oponnentScore;
-        this.playerScores = playerScores;
-        this.oponnentScores = oponnentScores;
+        this.opponentScore = opponentScore;
+        this.playerScoreRound1 = playerScores[0];
+        this.playerScoreRound2 = playerScores[1];
+        this.playerScoreRound3 = playerScores[2];
+        this.opponentScoreRound1 = opponentScores[0];
+        this.opponentScoreRound2 = opponentScores[1];
+        this.opponentScoreRound3 = opponentScores[2];
         this.winner = winner;
     }
 
-    public String getOponnentName() {
-        return OponnentName;
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getOpponentName() {
+        return opponentName;
+    }
+
+    public void setOpponentName(String opponentName) {
+        this.opponentName = opponentName;
     }
 
     public Date getDate() {
         return date;
     }
 
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
     public int getPlayerScore() {
         return playerScore;
     }
 
-    public int getOponnentScore() {
-        return oponnentScore;
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
     }
 
-    public int[] getPlayerScores() {
-        return playerScores;
+    public int getOpponentScore() {
+        return opponentScore;
     }
 
-    public int[] getOponnentScores() {
-        return oponnentScores;
+    public void setOpponentScore(int opponentScore) {
+        this.opponentScore = opponentScore;
+    }
+
+    public int getPlayerScoreRound1() {
+        return playerScoreRound1;
+    }
+
+    public void setPlayerScoreRound1(int playerScoreRound1) {
+        this.playerScoreRound1 = playerScoreRound1;
+    }
+
+    public int getPlayerScoreRound2() {
+        return playerScoreRound2;
+    }
+
+    public void setPlayerScoreRound2(int playerScoreRound2) {
+        this.playerScoreRound2 = playerScoreRound2;
+    }
+
+    public int getPlayerScoreRound3() {
+        return playerScoreRound3;
+    }
+
+    public void setPlayerScoreRound3(int playerScoreRound3) {
+        this.playerScoreRound3 = playerScoreRound3;
+    }
+
+    public int getOpponentScoreRound1() {
+        return opponentScoreRound1;
+    }
+
+    public void setOpponentScoreRound1(int opponentScoreRound1) {
+        this.opponentScoreRound1 = opponentScoreRound1;
+    }
+
+    public int getOpponentScoreRound2() {
+        return opponentScoreRound2;
+    }
+
+    public void setOpponentScoreRound2(int opponentScoreRound2) {
+        this.opponentScoreRound2 = opponentScoreRound2;
+    }
+
+    public int getOpponentScoreRound3() {
+        return opponentScoreRound3;
+    }
+
+    public void setOpponentScoreRound3(int opponentScoreRound3) {
+        this.opponentScoreRound3 = opponentScoreRound3;
     }
 
     public String getWinner() {
         return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
     }
 }
--- src/main/java/com/ap/gwentgame/client/model/User.java
+++ src/main/java/com/ap/gwentgame/client/model/User.java
@@ -3,24 +3,67 @@ package com.ap.gwentgame.client.model;
 import com.ap.gwentgame.client.enums.Question;
 import com.ap.gwentgame.client.model.gameElements.Card;
 
-
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
-    private  String name;
-    private  String password;
-    private  String nickName;
-    private  String email;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "games_played")
     private int gamesPlayed;
+
+    @Column(name = "highest_point")
     private int highestPoint;
+
+    @Column(name = "rank")
     private int rank;
+
+    @Column(name = "wins")
     private int wins;
+
+    @Column(name = "draws")
     private int draws;
+
+    @Column(name = "losses")
     private int losses;
-    private final Question question;
-    private final String answer;
-    private final ArrayList<Card> deck;
-    private final ArrayList<GameLog> gameHistory;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "question")
+    private Question question;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @ElementCollection
+    @CollectionTable(name = "user_deck", joinColumns = @JoinColumn(name = "user_id"))
+    private List<Card> deck;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private List<GameLog> gameHistory;
+
+    public User() {
+        this.deck = new ArrayList<>();
+        this.gameHistory = new ArrayList<>();
+    }
 
     public User(String name, String password, String nickName, String email, Question question, String answer) {
         this.name = name;
@@ -35,24 +78,93 @@ public class User {
         this.wins = 0;
         this.draws = 0;
         this.losses = 0;
-        this.deck = new ArrayList<Card>();
-        this.gameHistory = new ArrayList<GameLog>();
+        this.deck = new ArrayList<>();
+        this.gameHistory = new ArrayList<>();
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getNickName() {
         return nickName;
     }
 
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getHighestPoint() {
+        return highestPoint;
+    }
+
+    public void setHighestPoint(int highestPoint) {
+        this.highestPoint = highestPoint;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
     }
 
     public Question getQuestion() {
@@ -63,75 +175,11 @@ public class User {
         return answer;
     }
 
-    public int getGamesPlayed() {
-        return gamesPlayed;
-    }
-
-    public int getHighestPoint() {
-        return highestPoint;
-    }
-
-    public int getRank() {
-        return rank;
-    }
-
-    public int getWins() {
-        return wins;
-    }
-
-    public int getDraws() {
-        return draws;
-    }
-
-    public int getLosses() {
-        return losses;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setGamesPlayed(int gamesPlayed) {
-        this.gamesPlayed = gamesPlayed;
-    }
-
-    public void setHighestPoint(int highestPoint) {
-        this.highestPoint = highestPoint;
-    }
-
-    public void setRank(int rank) {
-        this.rank = rank;
-    }
-
-    public void setWins(int wins) {
-        this.wins = wins;
-    }
-
-    public void setDraws(int draws) {
-        this.draws = draws;
-    }
-
-    public void setLosses(int losses) {
-        this.losses = losses;
-    }
-
-    public ArrayList<Card> getDeck() {
+    public List<Card> getDeck() {
         return deck;
     }
 
-    public ArrayList<GameLog> getGameHistory() {
+    public List<GameLog> getGameHistory() {
         return gameHistory;
     }
 }
