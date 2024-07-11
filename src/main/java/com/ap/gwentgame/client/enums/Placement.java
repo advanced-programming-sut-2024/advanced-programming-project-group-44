5718978af281b10b1bd4b7e9cb04756ef29f345f
sobhanaghasi <sobhanaghasi1384@gmail.com>
2024-07-11 17:21:17 +0330

    Refined Placements

--- src/main/java/com/ap/gwentgame/client/enums/Placement.java
+++ src/main/java/com/ap/gwentgame/client/enums/Placement.java
@@ -3,15 +3,20 @@ package com.ap.gwentgame.client.enums;
 
 
 public enum Placement {
-    CLOSE_COMBAT,
-    RANGED_COMBAT,
-    SIEGE,
-    AGILE,
-    WEATHER,
-    SPECIAL_PLACE,
-    DECOY;
+    CLOSE_COMBAT(new int[]{0}),
+    RANGED_COMBAT(new int[]{1}),
+    SIEGE(new int[]{2}),
+    AGILE(new int[]{0, 1}),
+    WEATHER(new int[]{7}),
+    SPECIAL_PLACE(new int[]{4, 5, 6}),
+    DECOY(new int[]{0, 1, 2, 3, 4, 5, 6, 7});
 
     private int row;
+    private int[] allowedContainers;
+
+    Placement(int[] allowedContainers) {
+        this.allowedContainers = allowedContainers;
+    }
 
     public void setRow(int row) {
         this.row = row;
