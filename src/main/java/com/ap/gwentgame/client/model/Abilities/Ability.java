d4ca0793cdf9a4f2eb5ac0cac5d67903daa28b89
sobhanaghasi <sobhanaghasi1384@gmail.com>
2024-07-11 18:39:18 +0330

    Adapted Abilities

--- src/main/java/com/ap/gwentgame/client/model/Abilities/Ability.java
+++ src/main/java/com/ap/gwentgame/client/model/Abilities/Ability.java
@@ -4,19 +4,8 @@ import com.ap.gwentgame.client.model.gameElementViews.BoardView;
 import com.ap.gwentgame.client.model.gameElements.Card;
 
 public abstract class Ability {
-    private boolean hasAbilityInput = false;
-
     public Ability(){
     }
 
     public abstract void run(BoardView boardView, int index, Card card);
-
-    public void setAbilityInput() {
-        this.hasAbilityInput = true;
-    }
-
-    public boolean hasAbilityInput() {
-        return this.hasAbilityInput;
-    }
-
 }
